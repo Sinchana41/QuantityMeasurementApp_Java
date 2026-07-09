11a150d501a80914dbeb5783cf9cdca429c4b720
Sinchana41 <sinchanaan1234@gmail.com>
2026-07-09 16:08:38 +0530

    checking the equality of two numerical values

---
+++ src/main/java/com/quantitymeasurement/service/QuantityMeasurement.java
@@ -0,0 +1,10 @@
+package com.quantitymeasurement.service;
+
+import com.quantitymeasurement.model.Feet;
+
+public class QuantityMeasurement {
+
+    public boolean compare(Feet feet1 , Feet feet2){
+        return feet1.equals(feet2);
+    }
+}
